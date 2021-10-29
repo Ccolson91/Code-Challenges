4415c8d18ca245c97f9ea53e0889a1bd4896a29a
Christian Colson <christiancolson91@gmail.com>
2021-10-28 23:46:57 -0500

    code challenge upload

---
+++ printDigitsBackwards/printDigits.js
@@ -0,0 +1,12 @@
+// printDigits = num => {
+//   num = num + ""
+//   return num.split('').reverse().join("")
+// }
+
+// console.log(Number(printDigits(12345)))
+
+printDigits = num => {
+  while(){
+
+  }
+}
